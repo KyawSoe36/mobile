b7df2171fe65b0c183e251cfc672a0a679089639
Kyaw Soe <kyawsoe.cumdy2018@gmail.com>
2023-10-30 00:59:13 -0700

    added currency list and component

--- currency_convertor_app/App.js
+++ currency_convertor_app/App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Currencies from './src/components/Currencies';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Created App!</Text>
-      <StatusBar style="auto" />
+      <Currencies></Currencies>
     </View>
   );
 }
