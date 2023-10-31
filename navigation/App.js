0c76fa680f5b4b53ba3b688683dfd5fa279cdf66
Kyaw Soe <kyawsoe.cumdy2018@gmail.com>
2023-10-31 09:05:48 -0700

    added react navigation practice module

---
+++ navigation/App.js
@@ -0,0 +1,39 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './src/components/Home'
+import Details from './src/components/Details'
+
+
+const Stack = createNativeStackNavigator();
+
+
+function MyStack() {
+  return (
+
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Details" component={Details} />
+      </Stack.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+        <MyStack />
+    </NavigationContainer>
+
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
